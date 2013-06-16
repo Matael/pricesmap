9df70c54d6da2a09d6a32586e013f207b4d2724c
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-06-16 16:01:40 +0200

    View for detail page

--- pricesmap/webapp/views.py
+++ pricesmap/webapp/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import\
         get_object_or_404,\
         render_to_response,\
         RequestContext
+from django.http import Http404
 
-from views import *
-
+from pricesmap.webapp.models import *
+from pricesmap.webapp.forms import ItemForm
 
 def home(request):
     types = Produit.objects.all()
@@ -19,4 +20,21 @@ def home(request):
         context_instance=RequestContext(request)
     )
 
+def detail(request, type):
+    if not type in [_.url_name for _ in Produit.objects.all()]:
+        return Http404
+
+    produit = Produit.objects.get(url_name=type)
+    items = produit.item_set.all()
+    form = ItemForm()
+
+    return render_to_response(
+        'detail.html',
+        {
+            'produit': produit,
+            'items': items,
+            'form': form
+        },
+        context_instance=RequestContext(request)
+    )
 
