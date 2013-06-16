d3ccf247535103cfa15d5e9c50875fc51f368d15
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-06-16 19:35:49 +0200

    Correct URLs

--- pricesmap/webapp/urls.py
+++ pricesmap/webapp/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('pricesmap.webapp.views',
-    url(r'^product/(?P<type>\w+)/?', 'detail', name='detail'),
-    url(r'^product/(?P<type>\w+)/add/?', 'add', name='add'),
+    url(r'^product/(?P<type>\w+)/?$', 'detail', name='detail'),
+    url(r'^product/(?P<type>\w+)/add/?$', 'add', name='add'),
 )
 
 
