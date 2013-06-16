de7b1f0b2e409caf8bff7ef3a57074ed5382acb7
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-06-16 15:40:55 +0200

    Include webapps urls and specify r'^$'

--- pricesmap/urls.py
+++ pricesmap/urls.py
@@ -3,8 +3,18 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-     url(r'^/', include('pricesmap.webapp.urls')),
+from django.conf import settings
 
+from pricesmap.webapp.views import home
+
+
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('',
+    # for everything else, load url from webapp.urls
+    url(r'^pm/', include('pricesmap.webapp.urls')),
+    url(r'^/?$', home , name='home'),
+)
+
