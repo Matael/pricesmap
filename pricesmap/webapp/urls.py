93dd053c6506d850f10f90d7ededdf3aa3914762
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-07-21 22:19:42 +0200

    Add an About page$

--- pricesmap/webapp/urls.py
+++ pricesmap/webapp/urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls import patterns, include, url
 
+from django.views.generic.base import TemplateView
+
+class AboutView(TemplateView):
+
+    template_name = "about.html"
 urlpatterns = patterns('pricesmap.webapp.views',
     url(r'^product/(?P<type>\w+)/?$', 'detail', name='detail'),
     url(r'^product/(?P<type>\w+)/add/?$', 'add', name='add'),
 )
 
 
+urlpatterns += patterns('',
+    url(r'^about/?$', AboutView.as_view(), name="about")
+)
 
